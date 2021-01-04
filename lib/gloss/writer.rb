5bb97c912469c29a65ec3672b4dadefe1575f467
johansenja <43235608+johansenja@users.noreply.github.com>
2021-01-04 19:02:30 +0000

    Implement writer in gloss (#20)
    * Implement writer in gloss
    
    * Add writer specs

--- lib/gloss/writer.rb
+++ lib/gloss/writer.rb
@@ -1,27 +1,29 @@
 # frozen_string_literal: true
-
+require "pathname"
+require "fileutils"
 module Gloss
   module Utils
     module_function
-
     def src_path_to_output_path(src_path)
-      src_path.sub(%r{\A(?:\./)?#{Config.src_dir}/?}, "")
-              .sub(/\.gl$/, ".rb")
+      src_path.sub(/\A(?:\.\/)?#{Config.src_dir}\/?/, "")
+.sub(/\.gl$/, ".rb")
     end
   end
-
   class Writer
     include Utils
-
-    def initialize(content, src_path, output_path = nil)
-      @content, @src_path = content, src_path
-      @output_path = output_path || src_path_to_output_path(src_path)
+    def initialize(content, src_path, output_path =     Pathname.new(src_path_to_output_path(src_path))
+)
+      @content = content
+      @output_path = output_path
     end
-
-    def run
-      File.open(@output_path, "wb") do |file|
-        file << @content
+    def run()
+      unless       @output_path.parent
+.exist?
+        FileUtils.mkdir_p(@output_path.parent)
       end
+      File.open(@output_path, "wb") { |file|
+        file.<<(@content)
+      }
     end
   end
 end
